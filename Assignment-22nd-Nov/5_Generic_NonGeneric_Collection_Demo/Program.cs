685c3007cee3488a166d08254cd182a5b5450b23
Utsav-git <utsavsalokhe@gmail.com>
2021-11-22 20:07:13 +0530

    problem 5 added

---
+++ Assignment-22nd-Nov/5_Generic_NonGeneric_Collection_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace _5_Generic_NonGeneric_Collection_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable h = new Hashtable();
+            h.Add(1,"A");
+            h.Add(2, "B");
+
+            foreach (DictionaryEntry d in h) {
+                Console.WriteLine("Student Roll: "+d.Key+ " Student Name: "+d.Value);
+            }
+        }
+    }
+}
